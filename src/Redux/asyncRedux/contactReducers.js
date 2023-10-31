fd3cf636284e3d5a9aa8271135eae064ec2c4a18
Viktoriia <127604125+Viktiriia@users.noreply.github.com>
2023-10-31 15:10:48 +0200

    init

---
+++ src/Redux/asyncRedux/contactReducers.js
@@ -0,0 +1,53 @@
+import {  createSlice } from '@reduxjs/toolkit';
+import { fetchContacts, addContact, deleteContact } from './requestsAPI';
+
+const handlePanding = state => {
+  state.isLoading = true;
+};
+
+export const handleFulfilled = (state, action) => {
+  state.isLoading = false;
+  state.isError = null;
+  state.contacts = action.payload;
+};
+  
+export const handleRejected = (state, action) => {
+  state.isLoading = false;
+  state.isError = action.payload;
+};
+
+export const handleAddContactFulfilled = (state, action) => {
+  state.contacts.unshift(action.payload);
+  state.isLoading = false;
+  state.isError = null;
+};
+  
+  export const handleDeleteContactFulfilled = (state, action) => {
+    state.contacts = state.contacts.filter(
+      contact => contact.id !== action.payload.id
+    );
+    state.isLoading = false;
+    state.isError = null;
+  };
+  const contactsSlice = createSlice({
+    name: 'contacts',
+    initialState: {
+      contacts: [],
+      isLoading: false,
+      isError: null,
+    },
+    extraReducers: builder => {
+      builder
+        .addCase(fetchContacts.pending, handlePanding)
+        .addCase(fetchContacts.fulfilled, handleFulfilled)
+        .addCase(fetchContacts.rejected, handleRejected)
+        .addCase(addContact.pending, handlePanding)
+        .addCase(addContact.fulfilled, handleAddContactFulfilled)
+        .addCase(addContact.rejected, handleRejected)
+        .addCase(deleteContact.pending, handlePanding)
+        .addCase(deleteContact.fulfilled, handleDeleteContactFulfilled)
+        .addCase(deleteContact.rejected, handleRejected);
+    },
+  });
+  
+  export const newReducers = contactsSlice.reducer;
